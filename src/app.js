2255c64d8d673eac381fcce6af4afb619f681ba2
danoseun <ayodejisomefun@yahoo.com>
2020-02-02 17:16:24 +0100

    boostrap project

---
+++ src/app.js
@@ -0,0 +1,34 @@
+/* eslint-disable max-len */
+import dotenv from 'dotenv';
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+// import {
+//   defaultRouter, userRouter, protectedRouter, publicRouter
+// } from './routes';
+
+
+dotenv.config();
+
+const app = express();
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.use('/api', userRouter);
+app.use('/api', protectedRouter);
+app.use('/api', publicRouter);
+app.use('/api', defaultRouter);
+
+
+const port = process.env.PORT || 5500;
+
+app.listen(port, () => {
+  console.log(`Server is live on PORT ${port}`);
+});
+
+
+export default app;
